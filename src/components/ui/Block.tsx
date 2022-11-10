f86c2e44ab8b6d4cd2b7d115d29c45e932f00239
Al Joseph Condino <condinoaljoseph@gmail.com>
2022-11-10 16:34:18 +0800

    feat(components): add more components and hooks

---
+++ src/components/ui/Block.tsx
@@ -0,0 +1,61 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+interface Props {
+  children?: ReactNode;
+  className?: string;
+  slim?: boolean;
+  title?: string;
+  hideBottomBorder?: boolean;
+  label?: string;
+  labelTooltip?: string;
+  isCollapsable?: boolean;
+}
+
+export const Block = ({
+  children,
+  className = '',
+  slim = false,
+  title = '',
+  hideBottomBorder,
+  label,
+  labelTooltip,
+  isCollapsable,
+}: Props) => {
+  return (
+    <div
+      className={clsx(
+        'border-y border-skin-border bg-skin-block-bg text-base md:rounded-xl md:border',
+        className,
+      )}
+    >
+      {title !== '' ? (
+        <div
+          className={clsx(
+            'group flex h-[57px] justify-between rounded-t-none border-b border-skin-border px-4 pt-3 pb-[12px] md:rounded-t-lg',
+            {
+              'border-b-0': hideBottomBorder || isCollapsable,
+            },
+            { 'cursor-pointer': isCollapsable },
+          )}
+        >
+          <h4 className="flex items-center">
+            <div>{title}</div>
+          </h4>
+          <div className="flex items-center">
+            <div
+              className={clsx('text-xs text-skin-link', {
+                'cursor-help': labelTooltip,
+              })}
+            >
+              {label}
+            </div>
+          </div>
+        </div>
+      ) : null}
+      <div className={clsx('leading-5 sm:leading-6', { 'p-4': !slim })}>
+        {children}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/ui/Markdown.tsx
@@ -0,0 +1,38 @@
+import DOMPurify from 'isomorphic-dompurify';
+import { useEffect } from 'react';
+import { useCopy } from '../../composables/useCopy';
+import { marked } from 'marked';
+
+export const Markdown = ({ source = '' }: { source?: string }) => {
+  const { copyToClipboard } = useCopy();
+
+  useEffect(() => {
+    const body = document.querySelector('.markdown-body');
+    if (body !== null)
+      body.querySelectorAll('pre>code').forEach(function (code) {
+        const parent = code.parentElement;
+        if (parent !== null) parent.classList.add('rounded-lg');
+        const copyButton = document.createElement('button');
+        const copySpan = document.createElement('span');
+        copySpan.textContent = 'copy';
+        copySpan.classList.add('copy');
+        copySpan.classList.add('text-skin-text');
+        copyButton.appendChild(copySpan);
+        copyButton.addEventListener('click', function () {
+          if (parent !== null)
+            copyToClipboard(code.firstChild?.textContent?.trim());
+        });
+        code.appendChild(copyButton);
+      });
+  }, [source, copyToClipboard]);
+
+  return (
+    <div className="markdown-body break-words">
+      <div
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(marked.parse(source)),
+        }}
+      />
+    </div>
+  );
+};
